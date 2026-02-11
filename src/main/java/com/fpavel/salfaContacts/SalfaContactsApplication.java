a7fd3403e8fc5a66074ebde3e2da83978da7802d
Pavel-Fomchenkov <donotwritemail@gmail.com>
2026-02-11 14:36:47 +0300

    initial commit

---
+++ src/main/java/com/fpavel/salfaContacts/SalfaContactsApplication.java
@@ -0,0 +1,13 @@
+package com.fpavel.salfaContacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalfaContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalfaContactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpavel/salfaContacts/SalfaContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpavel.salfaContacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalfaContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
